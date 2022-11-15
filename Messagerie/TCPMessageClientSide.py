ce2f65dd7162b943407aeca3219aae663ce5a6fe
bololo7 <101951542+bololo7@users.noreply.github.com>
2022-11-15 22:23:32 +0100

    Finished

--- Messagerie/TCPMessageClientSide.py
+++ Messagerie/TCPMessageClientSide.py
@@ -1,4 +1,19 @@
 import socket
 
-host = "192.168.1.27"
+host = "127.0.0.1"
 port = 9990
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host,port))
+
+while True:
+    flow = input('->')
+    client.send(flow.encode('utf-8'))
+
+    server_request = client.recv(400)
+    server_request = server_request.decode('utf-8')
+    print('Received {}'.format(server_request))
+
+
+
+
--- Messagerie/TCPMessageServerSide.py
+++ Messagerie/TCPMessageServerSide.py
@@ -1,12 +1,25 @@
 import socket
 
-host = "192.168.1.20"
+host = "127.0.0.1"
 port = 9990
 
 #Socket creation
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind((host,port))
-socket.listen(1)
-client_socket, address = socket.accept()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen(1)
+print(f'Listening on {host}:{port}')
+client_socket, address = server.accept()
 print(f'Connection {address[0]}:{address[1]}')
+
+while True:
+    request = client_socket.recv(400)
+    print('Received {}'.format(request.decode('utf-8')))
+    if not request : #if client sending is empty
+        print('Close')
+        break
+    msg = input("->")
+    client_socket.send(msg.encode('utf-8'))
+
+client_socket.close()
+server.close()
 
