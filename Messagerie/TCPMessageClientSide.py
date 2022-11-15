2182b1381a352c40edb2774894a60966f1a7d065
bololo7 <101951542+bololo7@users.noreply.github.com>
2022-11-15 21:35:02 +0100

    Add and update

---
+++ Messagerie/TCPMessageClientSide.py
@@ -0,0 +1,4 @@
+import socket
+
+host = "192.168.1.27"
+port = 9990
--- Messagerie/TCPMessageServerSide.py
+++ Messagerie/TCPMessageServerSide.py
@@ -1,1 +1,12 @@
 import socket
+
+host = "192.168.1.20"
+port = 9990
+
+#Socket creation
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind((host,port))
+socket.listen(1)
+client_socket, address = socket.accept()
+print(f'Connection {address[0]}:{address[1]}')
+
