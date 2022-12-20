c134014442a4c116e13973770e050f236b1f8bb6
bololo7 <101951542+bololo7@users.noreply.github.com>
2022-12-20 20:45:39 +0100

    Fixed

--- Netcat.py
+++ Netcat.py
@@ -1,4 +1,5 @@
 """Netcat replacement in python3
+Project: Black Hat Python 2nd edition from Justin Seitz & Tim Arnold-3
 """
 import argparse
 import socket
@@ -14,34 +15,6 @@ def execute(cmd):
         return
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)    #return output of a command executed on the OS
     return output.decode()
-
-# Creating of command line interface
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Netcat Replacement in Python3',
-    formatter_class=argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent(
-        '''Example:
-        netcat.py -t 192.168.1.108 -p 5555 -l -c #command shell
-        netcat.py -t 192.168.1.108 -p 5555 -l -u=mytext.txt #upload to file
-        netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" #execute commande
-        echo 'ABC' | ./netcat.py -l 192.168.1.108 -p 135 #echo text to server port 135
-        netcat.py -t 192.168.1.108 -p 5555 #connect to server
-    '''))
-parser.add_argument('-c','--command', action='store_true', help='command shell')
-parser.add_argument('-e', '--execute',help='execute specified command')
-parser.add_argument('-l', '--listen', action='store_true',help='listen')
-parser.add_argument('-p', '--port', type=int, default=5555, help='speicied port')
-parser.add_argument('-t','--target', default='192.168.1.203', help='specified IP')
-parser.add_argument('-u','--upload', help='upload file')
-args = parser.parse_args()
-if args.listen:
-    buffer = ''
-else :
-    buffer = sys.stdin.read()
-
-
-nc = NetCat(args, buffer.encode())
-nc.run()
 
 class NetCat:
     def __init__(self, args, buffer=None):
@@ -120,3 +93,30 @@ class NetCat:
                     print(f'server killed {e}')
                     self.socket.close()
                     
+# Creating of command line interface
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Netcat Replacement in Python3',
+    formatter_class=argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent(
+        '''Example:
+        netcat.py -t 192.168.1.108 -p 5555 -l -c #command shell
+        netcat.py -t 192.168.1.108 -p 5555 -l -u=mytext.txt #upload to file
+        netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" #execute commande
+        echo 'ABC' | ./netcat.py -l 192.168.1.108 -p 135 #echo text to server port 135
+        netcat.py -t 192.168.1.108 -p 5555 #connect to server
+    '''))
+parser.add_argument('-c','--command', action='store_true', help='command shell')
+parser.add_argument('-e', '--execute',help='execute specified command')
+parser.add_argument('-l', '--listen', action='store_true',help='listen')
+parser.add_argument('-p', '--port', type=int, default=5555, help='speicied port')
+parser.add_argument('-t','--target', default='192.168.1.203', help='specified IP')
+parser.add_argument('-u','--upload', help='upload file')
+args = parser.parse_args()
+if args.listen:
+    buffer = ''
+else :
+    buffer = sys.stdin.read()
+
+
+nc = NetCat(args, buffer.encode())
+nc.run()
