218b274040616bdee955240ab75d46539ba0dc05
bololo7 <101951542+bololo7@users.noreply.github.com>
2022-12-20 21:05:48 +0100

    Fixed

--- Netcat.py
+++ Netcat.py
@@ -105,18 +105,18 @@ if __name__ == '__main__':
         echo 'ABC' | ./netcat.py -l 192.168.1.108 -p 135 #echo text to server port 135
         netcat.py -t 192.168.1.108 -p 5555 #connect to server
     '''))
-parser.add_argument('-c','--command', action='store_true', help='command shell')
-parser.add_argument('-e', '--execute',help='execute specified command')
-parser.add_argument('-l', '--listen', action='store_true',help='listen')
-parser.add_argument('-p', '--port', type=int, default=5555, help='speicied port')
-parser.add_argument('-t','--target', default='192.168.1.203', help='specified IP')
-parser.add_argument('-u','--upload', help='upload file')
-args = parser.parse_args()
-if args.listen:
-    buffer = ''
-else :
-    buffer = sys.stdin.read()
+    parser.add_argument('-c','--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute',help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true',help='listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='speicied port')
+    parser.add_argument('-t','--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-u','--upload', help='upload file')
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else :
+        buffer = sys.stdin.read()
 
 
-nc = NetCat(args, buffer.encode())
-nc.run()
+    nc = NetCat(args, buffer.encode())
+    nc.run()
